7365da18b1ec865370ced5e71ddc2d983c1d77c5
judithkuneth <judith.kuneth@gmail.com>
2022-10-25 19:09:58 +0200

    edit port

--- api/server.js
+++ api/server.js
@@ -33,7 +33,7 @@ app.get("/", function (req, res, next) {
 
 // app.listen(app.listen(process.env.PORT || 3000));
 
-app.listen(process.env.PORT || 3002, function () {
+app.listen(process.env.PORT, function () {
   console.log("CORS-enabled web server listening on port 3002");
 });
 
