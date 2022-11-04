0c375c31675c06a20935061c2841ebc9905a7dd4
judithkuneth <judith.kuneth@gmail.com>
2022-11-04 12:13:58 +0100

    edit getProductsFromMongoDb

--- store/products.js
+++ store/products.js
@@ -94,7 +94,7 @@ export const actions = {
   // Products
   getProductsFromMongoDb({ commit }) {
     axios
-      .get("http://localhost:3002/products")
+      .get("/products")
       .then((res) => {
         console.log(res);
         commit("updateProducts", res.data);
